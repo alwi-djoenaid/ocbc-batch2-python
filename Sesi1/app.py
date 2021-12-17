29ae689913a59d38e62b29acb5bed476a0c698c4
alwi-djoenaid <alwi.djoenaid@gmail.com>
2021-12-17 13:59:10 +0700

    init

---
+++ Sesi1/app.py
@@ -0,0 +1,17 @@
+#print(10 + 11)
+
+# name = "Hacktiv8"
+# age = 22
+# hasLaptop = True
+
+# print(name, age, hasLaptop)
+
+# a = 4
+# b = 3
+# print(a == b)
+
+# s = 'foo'
+# t = 'bar'
+# print(s + t)
+# print(s * 4)
+
