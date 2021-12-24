5996855bd233c5c020f55fec37ca771ed900a96c
alwi-djoenaid <alwi.djoenaid@gmail.com>
2021-12-24 11:19:50 +0700

    added sesi11

---
+++ Sesi11/config.py
@@ -0,0 +1,23 @@
+import os
+import connexion
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Create the Connexion application instance
+connex_app = connexion.App(__name__, specification_dir=basedir)
+
+# Get the underlying Flask app instance
+app = connex_app.app
+
+# Configure the SQLAlchemy part of the app instance
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ouvgnvctiojsjh:a45cf8d9bf673db901cd20ca21ae14dd356806d870716882fc173ea50ec66b1c@ec2-52-44-50-220.compute-1.amazonaws.com:5432/dfq812b2k53qh1'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create the SQLAlchemy db instance
+db = SQLAlchemy(app)
+
+# Initialize Marshmallow
+ma = Marshmallow(app)
